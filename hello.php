348f25a5581909ff1a7e3891b3bae1a02c07f29c
puchcharapoom-mangmool <6206021610111@fitm.kmutnb.ac.th>
2021-01-13 09:58:32 +0700

    create hello.php

---
+++ hello.php
@@ -0,0 +1,13 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewpoint" content="width=dwevice-width,initial-scale=1.0">
+        <title>Hello</title>
+    </head>
+    <body>
+       <?php
+            echo ("<h1> Hello </h1> <br> ") ;
+            echo ("<h2> Prachinburi </h2>") ;
+        ?>
+    </body>
+</html>
